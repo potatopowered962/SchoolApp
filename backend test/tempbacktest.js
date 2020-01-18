25e2fa0efdbaa82b7088c0c351592ac3399186f8
NKay15 <60024623+NKay15@users.noreply.github.com>
2020-01-18 13:07:42 +0800

    Update tempbacktest.js
    Fix some stuff

--- backend test/tempbacktest.js
+++ backend test/tempbacktest.js
@@ -1,11 +1,15 @@
-import SignUpButton from 'ConvolverNode.js'
+import SignUpButton from 'core.js'
 
 export function getSignUpButton()
 {
     var ret = []
-    ret.push(new SignUpButton('Extra Class','To Make you suffer'))
+    var a = new SignUpButton('Extra Class','To Make you suffer')
+    ret.push(a)
+    delete a
     ret[0].addelective('A','Subject A')
     ret[0].addelective('B','Subject B')
-    ret.push(new SignUpButton('Games','Inter Class Game'))
+    var b = new SignUpButton('Games','Inter Class Game')
+    ret.push(b)
+    delete b
     return ret
 }
