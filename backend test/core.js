5bf73a34acf604b7375ebc4cef7b97d7b44aea81
NKay15 <60024623+NKay15@users.noreply.github.com>
2020-01-18 15:29:46 +0800

    Update core.js to include event

--- backend test/core.js
+++ backend test/core.js
@@ -75,3 +75,100 @@ export class SignUpButton
     }
 
 }
+
+// Event Page
+export class EventList
+{
+
+    constructor(name, description, date)
+    {
+        this.name = name
+        this.description = description
+        this.date = date
+    }
+
+    name()
+    {
+        return this.name
+    }
+
+    description()
+    {
+        return this.description
+    }
+
+    day()
+    {
+        return this.date[0]
+    }
+
+    month()
+    {
+        return this.date[1]
+    }
+
+    year()
+    {
+        return this.date[2]
+    }
+
+    month_w()
+    {
+        switch(this.date[1])
+        {
+            case 1:
+                return 'January'
+                break;
+            case 2:
+                return 'February'
+                break;
+            case 3:
+                return 'March'
+                break;
+            case 4:
+                return 'April'
+                break;
+            case 5:
+                return 'May'
+                break;
+            case 6:
+                return 'June'
+                break;
+            case 7:
+                return 'July'
+                break;
+            case 8:
+                return 'August'
+                break;
+            case 9:
+                return 'September'
+                break;
+            case 10:
+                return 'October'
+                break;
+            case 11:
+                return 'November'
+                break;
+            case 12:
+                return 'December'
+                break;
+            default:
+                return null
+        }
+    }
+
+    date1()   //01-01-2020
+    {
+        var ret = (date[0]).toString()
+        ret.concat('-',(date[1]).toString(),'-',(date[2]).toString())
+        return ret
+    }
+
+    date2()   //01-January-2020
+    {
+        var ret = (date[0]).toString()
+        ret.concat(' ',this.month_w(),' ',(date[2]).toString())
+        return ret
+    }
+
+}
