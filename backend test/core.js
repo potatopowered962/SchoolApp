cc7aa06790f720b38e9e6b7ee20bba376b968124
NKay15 <60024623+NKay15@users.noreply.github.com>
2020-01-18 16:10:18 +0800

    Fixed

--- backend test/core.js
+++ backend test/core.js
@@ -1,3 +1,82 @@
+// Sign Up Page Buttons
+class SignUpElective
+{
+    constructor(name, description)
+    {
+        this.name = name
+        this.description = description
+    }
+
+    name()
+    {
+        return this.name
+    }
+
+    description()
+    {
+        return this.description
+    }
+
+}
+
+export class SignUpButton
+{
+
+    constructor(name, description)
+    {
+        this.name = name
+        this.description = description
+        this.elective = []
+    }
+
+    addelective(name, description)
+    {
+        var t_elective = new SignUpElective(name, description)
+        this.elective.push(t_elective)
+    }
+
+    name()
+    {
+        return this.name
+    }
+
+    description()
+    {
+        return this.description
+    }
+
+    electivecount()
+    {
+        return this.elective.length
+    }
+
+    electivename(i)
+    {
+        if (i < this.electivecount())
+        {
+            return ''
+        }   
+
+        else{
+            return this.elective[i].name
+        }
+    }
+
+    electivedescription(i)
+    {
+        if (i < this.electivecount())
+        {
+            return ''
+        }   
+
+        else{
+         return this.elective[i].description
+        }
+    }
+
+}
+
+// Event Page
 export class EventList
 {
 
