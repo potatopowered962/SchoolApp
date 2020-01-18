91365b60fb85c2e7e53930f080e519f4929fe46a
NKay15 <60024623+NKay15@users.noreply.github.com>
2020-01-18 14:05:38 +0800

    Update tempbacktest.js
    Fixed + Suggestion

--- backend test/tempbacktest.js
+++ backend test/tempbacktest.js
@@ -1,15 +1,27 @@
-import SignUpButton from 'core.js'
+import { SignUpButton } from './core.js'
 
 export function getSignUpButton()
 {
     var ret = []
     var a = new SignUpButton('Extra Class','To Make you suffer')
     ret.push(a)
-    delete a
     ret[0].addelective('A','Subject A')
     ret[0].addelective('B','Subject B')
     var b = new SignUpButton('Games','Inter Class Game')
     ret.push(b)
-    delete b
     return ret
+
+/*
+    for(var i = 0; i < z; i++)
+    {
+        var temp = new SignUpButton('name','descrtiption')
+        if (elective)
+        {
+            // for looop
+            // ret[i].addelective()
+        }
+        ret.push(a)
+    }
+*/
+
 }
