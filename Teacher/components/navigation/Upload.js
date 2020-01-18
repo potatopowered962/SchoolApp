5f1a55620632dba6641865f73e05b3ad078e7378
Yuxuan <huang7158b@tmjc.edu.sg>
2020-01-18 16:46:00 +0800

    hehheh

--- Teacher/components/navigation/Upload.js
+++ Teacher/components/navigation/Upload.js
@@ -1,35 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import { 
   StyleSheet,
   Text,
   View, 
   Button,
   Alert,
-  
+  AppRegistry,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+import ImagePicker from "react-native-image-picker";
 
 
-export default function Upload() {
-
+export default class Upload extends React.Component {
+  state={photo:null,}
+ handleChoosePhoto=()=> {
+   const options = {noData :true };
+   ImagePicker.launchImageLibrary(options,response=>{
+     console.log("response",response);
+     }
+   )
+ }
+ render(){
+ const {photo} = this.state;
     return (
-    <View style={styles.container}>
+    <View style={{  flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',}}>
       <View> 
         <Button
           title="Upload timetable"
-          color="rgb(50,100,100)"
-          onPress={() => Alert.alert('Uploaded')}
-        /></View>
+          
+          onPress={this.handleChoosePhoto.bind(this)}
+        />
+        </View>
       
+    
     </View>
 
     )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#cadefc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+}
