270267280e8f6335ed3916086088531affa453b9
gohzh7801 <60023892+gohzh7801@users.noreply.github.com>
2020-01-18 16:48:32 +0800

    Authentication added

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Navigator from './LoginStack';
 import Amplify from 'aws-amplify';
 import amplify from './YOUR-PATH-TO/aws-exports';
+import { withAuthenticator } from 'aws-amplify-react-native';
 Amplify.configure(amplify);
 
-export default function App() {
+export default withAuthenticator(App) {
   return (
     <Navigator/>
   );
