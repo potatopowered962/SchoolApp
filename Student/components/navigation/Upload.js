4c273f7425a9e19ca298b52bedc1078782dd7401
Yuxuan <huang7158b@tmjc.edu.sg>
2020-01-19 09:47:21 +0800

    igh

--- Student/components/navigation/Upload.js
+++ Student/components/navigation/Upload.js
@@ -1,35 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import { 
   StyleSheet,
   Text,
   View, 
   Button,
   Alert,
-  
+  AppRegistry,
+  TouchableOpacity,
+  Image,
 } from 'react-native';
+import { RNCamera } from 'react-native-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 
-export default function Upload() {
+
+export default class Upload extends React.Component {
+  state = {
+    image: null,
+  };
+
+  render() {
+    let { image } = this.state;
 
     return (
-    <View style={styles.container}>
-      <View> 
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Button
-          title="Upload timetable"
-          color="rgb(50,100,100)"
-          onPress={() => Alert.alert('Uploaded')}
-        /></View>
-      
-    </View>
+          title="Upload"
+          onPress={this._pickImage}
+        />
+        {image &&
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </View>
+    );
+  }
 
-    )
+  componentDidMount() {
+    this.getPermissionAsync();
+    console.log('hi');
+  }
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  }
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#cadefc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
